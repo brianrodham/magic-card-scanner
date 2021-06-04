d9939612bae3af7d7d8e1e4d424b3c96e6a8bc0e
Brian Rodham <brian.j.rodham@gmail.com>
2021-06-03 23:47:13 -0600

    Fetch images based on image_uris

--- fetch-cards.py
+++ fetch-cards.py
@@ -1,20 +1,36 @@
 import json
+import requests
 
-#f = open('./default-cards.json')
-#data = json.load(f)
+def DownloadImage(url):
+    img_data = requests.get(url).content
+    image_name =  url.split('/')[-1]
+    image_name = image_name.split("?", 1)[0]
+    image_url = "E:/magic-images/originals/" + image_name
+    with open(image_url, 'wb') as handler:
+        handler.write(img_data)
 
 with open("./default-cards.json", encoding="utf8") as file:
     reader = json.load(file)
+    i = 0;
     for card in reader:
-        print(card["name"])
-#print(len(data))
-#for card in data:
-#    print(card.name)
+        if("image_uris" in card):
+            print(card["name"], card["image_uris"]["normal"])
+            # Get all useful information
+            id = card.get("id")
+            tcgplayer_id = card.get("tcgplayer_id")
+            name = card.get("name")
+            url = card["image_uris"].get("normal")
+
+            # Download image
+            DownloadImage(url)
+            
+            
+            # Temporary sanity check to work with a smaller dataset
+            i += 1
+            if(i >= 10):
+                break
+
+            # Store useful data, including image name in a csv file for later use
 
 
-#def DownloadImage(url):
-#    img_data = requests.get(url).content
-#    image_name =  url.split('/')[-1]
-#    image_url = "./output/" + image_name
-#    with open(image_url, 'wb') as handler:
-#        handler.write(img_data)
+
