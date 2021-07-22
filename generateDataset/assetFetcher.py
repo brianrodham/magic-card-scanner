850c8ec01be74a5022df07066e46e4b33d9b69d8
Brian Rodham <brian.j.rodham@gmail.com>
2021-07-21 21:28:22 -0600

    Removed seperate asset downloader class

--- generateDataset/assetFetcher.py
+++ generateDataset/assetFetcher.py
@@ -1,13 +1,12 @@
+from generateDataset.imageNameHelper import ImageNameHelper
 import json
 from .csvWriter import CsvWriter
-from .assetDownloader import AssetDownloader
 from .assetAugmentor import AssetAugmentor
-
-
+from .imageNameHelper import ImageNameHelper
+import requests
 class AssetFetcher:
 
     csvWriter = CsvWriter()
-    assetDownloader = AssetDownloader()
 
     def Fetch(self):
         self.csvWriter.ResetCSVFile()
@@ -19,7 +18,7 @@ class AssetFetcher:
                     print(card["name"], card["image_uris"]["normal"])
                     url = card["image_uris"].get("normal")
                     # Download image
-                    self.assetDownloader.DownloadImage(url)
+                    self.DownloadImage(url)
 
                     # Store useful data, including image name in a csv file for later use
                     self.csvWriter.WriteCSVRecord(card)
@@ -30,6 +29,13 @@ class AssetFetcher:
                         break
         assetAugmentor = AssetAugmentor()
         assetAugmentor.Augment(100)
+
+    def DownloadImage(self, url):
+        img_data = requests.get(url).content
+        image_name =  ImageNameHelper.GetImageName(url)
+        image_url = "E:/magic-images/originals/" + image_name
+        with open(image_url, 'wb') as handler:
+            handler.write(img_data)
 
 
 
