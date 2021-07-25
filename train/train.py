3eb1d347694ee5aa14b78d3ef8f3e93025f228c0
Brian Rodham <brian.j.rodham@gmail.com>
2021-07-25 00:25:24 -0600

    Split values into training and validation sets

--- train/train.py
+++ train/train.py
@@ -14,3 +14,26 @@ data_dir = pathlib.Path(dataset_url)
 
 image_count = len(list(data_dir.glob('*/*.jpg')))
 print(image_count)
+
+batch_size = 32
+img_height = 680
+img_width = 488
+
+train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+  data_dir,
+  validation_split=0.2,
+  subset="training",
+  seed=123,
+  image_size=(img_height, img_width),
+  batch_size=batch_size)
+
+val_ds = tf.keras.preprocessing.image_dataset_from_directory(
+    data_dir,
+    validation_split=0.2,
+    subset="validation",
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+class_names =  train_ds.class_names
+print(class_names)
