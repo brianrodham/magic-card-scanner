deba724dfd0f38c38e15d1d88bf5922cacd5f7a8
Brian Rodham <brian.j.rodham@gmail.com>
2021-08-08 14:39:15 -0600

    Prepared training code for v1

--- train/train.py
+++ train/train.py
@@ -8,6 +8,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
+# Load the data from disk
 import pathlib
 dataset_url = "E:/magic-images/augmented"
 data_dir = pathlib.Path(dataset_url)
@@ -15,10 +16,12 @@ data_dir = pathlib.Path(dataset_url)
 image_count = len(list(data_dir.glob('*/*.jpg')))
 print(image_count)
 
+# Some basic info
 batch_size = 32
 img_height = 680
 img_width = 488
 
+# Create training dataset
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
   data_dir,
   validation_split=0.2,
@@ -27,6 +30,7 @@ train_ds = tf.keras.preprocessing.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
+# Create validation dataset
 val_ds = tf.keras.preprocessing.image_dataset_from_directory(
     data_dir,
     validation_split=0.2,
@@ -35,5 +39,42 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
     image_size=(img_height, img_width),
     batch_size=batch_size)
 
-class_names =  train_ds.class_names
-print(class_names)
+
+num_classes = len(train_ds.class_names)
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+
+# Standardize the data
+normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+
+# Create the model
+model = tf.keras.Sequential([
+  normalization_layer,
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(num_classes)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# Train the model
+epochs=10
+history = model.fit(
+  train_ds,
+  validation_data=val_ds,
+  epochs=epochs
+)
+
+model.save('../models/mtg_card_model.h5')
